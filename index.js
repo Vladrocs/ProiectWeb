88180ac853d45c95835cd121774e8af435d5aa21
vladrocs <vlad.morutan98@e-uvt.ro>
2020-12-05 17:53:24 +0200

    add mongodb support ( strange error) + nodemon)

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var app = require('express')();
-var express = require('express');
+const express = require('express');
+var app = express();
 const url = require('url');
-var fs = require('fs');//to acces file from fs
+const fs = require('fs');//to acces file from fs
 var http = require('http').createServer(app);
+const mongo = require("mongodb");
+var MongoClient = mongo.MongoClient;
+
+const uri ="mongodb://localhost:27017/";
+//const client = new MongoClient(uri);
+const client = new MongoClient(uri);
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,9 +20,25 @@ app.get('/', function(req, res){
 app.get('/inregistrare.html', function(req, res){
   res.sendFile(__dirname + '\\public\\html\\inregistrare.html');
   console.log('--------------------------');
-  const urlVars = url.parse(req.url,true).query;
-  //console.log(urlVars);    datele din formul de inregistrare.
+  var urlVars = url.parse(req.url,true).query;
+  //urlVars=JSON.parse(JSON.stringify(urlVars)); //req.body = [Object: null prototype] -- elimina acest lucru.
+  //console.log(myobj);
+  //console.log(urlVars);
+    MongoClient.connect(uri, function(err, db) {
+    if (err) throw err;
+    var dbc = db.db("messenger");
+    var myobj = { name: "Company Inc", address: "Highway 37" };
+    dbc.collection("conturi").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+
+
 });
+
+
 
 
 
