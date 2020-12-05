bca646b00d07284bc3187f4ca05294a37e9f67c6
vladrocs <vlad.morutan98@e-uvt.ro>
2020-12-05 15:47:14 +0200

    preluare parametrii de la formul de inregistrare

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 var app = require('express')();
 var express = require('express');
+const url = require('url');
 var fs = require('fs');//to acces file from fs
 var http = require('http').createServer(app);
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    console.log("--------------------------------------------------------------------");
   res.sendFile(__dirname + '\\public\\html\\login.html');
 });
 
 app.get('/inregistrare.html', function(req, res){
-    console.log("--------------------------------------------------------------------");
   res.sendFile(__dirname + '\\public\\html\\inregistrare.html');
+  console.log('--------------------------');
+  const urlVars = url.parse(req.url,true).query;
+  //console.log(urlVars);    datele din formul de inregistrare.
 });
+
+
+
+
+
+
 
 http.listen(80, () => {
   console.log('listening on *:80');
