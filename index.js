224ac807ba400492f7491d503e16e35c86dab086
iulian <raduicaiulian@gmail.com>
2020-12-06 22:05:27 +0200

    login+citire DB

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
 const express = require('express');
-var app = express();
+const app = express();
 const url = require('url');
 const fs = require('fs');//to acces file from fs
 var http = require('http').createServer(app);
 const mongo = require("mongodb");
 var MongoClient = mongo.MongoClient;
 
-const uri ="mongodb://localhost:27017/";
-//const client = new MongoClient(uri);
+const uri ="mongodb://localhost:27017";
 const client = new MongoClient(uri);
-
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '\\public\\html\\login.html');
+//login
+var urlVars = url.parse(req.url,true).query;
+urlVars=JSON.parse(JSON.stringify(urlVars));
+MongoClient.connect(uri, function(err, db) {
+    var dbc = db.db("messenger");
+    obj={username: urlVars["username"], password: urlVars["password"]};
+    dbc.collection("conturi").find(obj).toArray(function(err, result) {
+      if (err) throw err;
+      if(result.length==0){
+        console.log("credentiale gresite");
+        //redirect la home( work in progress)
+        //res.sendFile(__dirname + '\\public\\html\\home.html');
+        res.redirect("/public/html/ home.html");
+        res.end();
+      }else{
+        console.log("credentiale acceptate");
+        //pop-up credentiale gresite(to do)
+      
+      }
+      db.close();
+    });
+    db.close();
+  });
 });
 
-app.get('/inregistrare.html', function(req, res){
+app.get('/inregistrare', function(req, res){
   res.sendFile(__dirname + '\\public\\html\\inregistrare.html');
   console.log('--------------------------');
   var urlVars = url.parse(req.url,true).query;
-  //urlVars=JSON.parse(JSON.stringify(urlVars)); //req.body = [Object: null prototype] -- elimina acest lucru.
+  urlVars=JSON.parse(JSON.stringify(urlVars)); //req.body = [Object: null prototype] -- elimina acest lucru.
   //console.log(myobj);
   //console.log(urlVars);
-    MongoClient.connect(uri, function(err, db) {
+  MongoClient.connect(uri, function(err, db) {
     if (err) throw err;
     var dbc = db.db("messenger");
     var myobj = { name: "Company Inc", address: "Highway 37" };
-    dbc.collection("conturi").insertOne(myobj, function(err, res) {
+    dbc.collection("conturi").insertOne(urlVars, function(err, res) {
       if (err) throw err;
       console.log("1 document inserted");
       db.close();
     });
   });
-
-
 });
-
-
-
-
-
-
 
 
 
